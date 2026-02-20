311830a883c4de27969d914167b232a1778443ee
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-02-20 16:36:46 +0300

    change load method and ordering in the DB

--- src/main/java/org/example/exception/InvestmentAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package org.example.exception;
-
-public class InvestmentAlreadyExistsException extends RuntimeException {
-    public InvestmentAlreadyExistsException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
-
